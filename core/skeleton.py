0294aae15168cdf305dcc2a609c04c911806eb9d
Sven Eberth <se@mausbrand.de>
2022-11-02 14:32:31 +0100

    Change stuff in skeleton

--- core/skeleton.py
+++ core/skeleton.py
@@ -953,11 +953,11 @@ class Skeleton(BaseSkeleton, metaclass=MetaSkel):
                 oldBlobLockObj["is_stale"] = False
                 db.Put(oldBlobLockObj)
             else:  # We need to create a new blob-lock-object
-                blobLockObj = db.Entity(db.Key("viur-blob-locks", dbObj.key.id_or_name))
+                blobLockObj = db.Entity(db.Key("viur-blob-locks",    dbObj.key.id_or_name))
                 blobLockObj["active_blob_references"] = list(blobList)
                 blobLockObj["old_blob_references"] = []
-                blobLockObj["has_old_blob_references"] = False
-                blobLockObj["is_stale"] = False
+                blobLockObj["has_old_blob_references"] =   False
+                blobLockObj["is_stale"] =    False
                 db.Put(blobLockObj)
 
             return dbObj.key, dbObj, skel, changeList
